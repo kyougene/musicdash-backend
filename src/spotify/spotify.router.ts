5122bf3efcc4d826f9582dbe37b309110a68965e
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 11:55:00 +0900

    fix

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -4,7 +4,7 @@ export const spotifyRouter = express.Router();
 
 spotifyRouter.get("/top", async (req, res) => {
   const accessToken =
-    "BQA0G2w_c8j1AJjJ6fLtdE_LjEFf1_UBk1KdiJUT0FmhsRWLQjqI9aJZAQSu_JmYECwCJYijOCTTCS12qLknA3Rr8pxwRFycjrVTaAq14FaqqCj9zUxAwCj5uEHUSmBb0Bs6qlbdJUP8-Rs3pZUFqlyPHpH-CygA58WMqHaYFWh2bofBBahtmbzafdUdczEoayWEhqM";
+    "AQA-G788WVQDJTBVmn6HUAI3xWs7yEib_3vSUT7PE9N7RSwGO-ziIVyG4hA5nZ0KKQ0FhgxlSCqVwmIkxifU40ocv5gJUKnxqlcdOYO1A3Rxq7rn1lvhYlemPMhOTFul5tc ";
 
   const url = "https://api.spotify.com/v1/me/top/artists";
 
