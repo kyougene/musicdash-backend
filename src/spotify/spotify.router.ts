b0d62ce8c746a77e3ccc360cb898efb116467a43
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 15:52:33 +0900

    fix

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -4,8 +4,6 @@ import { UserAuthInfoRequest } from "../custom.js";
 export const spotifyRouter = express.Router();
 
 spotifyRouter.get("/top", (req: UserAuthInfoRequest, res) => {
-  if (req.user) {
-    console.log(req.user);
-  }
-  res.send(req.session);
+  console.log(req.user);
+  res.send(req.user);
 });
