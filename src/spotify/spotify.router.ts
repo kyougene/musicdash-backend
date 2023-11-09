80829d840f8ae9be6dfc060d60c06119cfc11128
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 10:59:55 +0900

    fix

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -3,6 +3,6 @@ import express from "express";
 export const spotifyRouter = express.Router();
 
 spotifyRouter.get("/recents", (req, res) => {
-  const user = req.user;
+  const user = req.session.user;
   console.log(user);
 });
