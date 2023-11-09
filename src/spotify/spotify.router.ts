249d8c99628162389c0d82b0caba366eafa34e36
Kai Eugene <kai.yougene@gmail.com>
2023-11-10 01:55:50 +0900

    spotify endpoints

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -5,8 +5,6 @@ import axios from "axios";
 export const spotifyRouter = express.Router();
 
 spotifyRouter.get("/top", async (req: UserInfo, res) => {
-  console.log(req.user);
-  console.log(req.user.spotifyId);
   const id = req.user.spotifyId;
   const user = await prisma.user.findFirst({
     where: {
@@ -33,10 +31,69 @@ spotifyRouter.get("/top", async (req: UserInfo, res) => {
     const topTracks = response.data.items.map((data) => {
       return data.name;
     });
-    console.log(topTracks);
     res.json(topTracks);
   } catch (error) {
     console.error("Error fetching top tracks:", error);
     res.status(500).json({ error: "Failed to fetch top tracks" });
+  }
+});
+
+spotifyRouter.get("/profile", async (req: UserInfo, res) => {
+  const id = req.user.spotifyId;
+  const user = await prisma.user.findFirst({
+    where: {
+      spotifyId: id,
+    },
+  });
+  const accessToken = user.accessToken;
+  const url = `https://api.spotify.com/v1/users/${user.spotifyId}`;
+  const options = {
+    method: "GET",
+    url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+
+  try {
+    const profile = await axios(options);
+
+    res.json(profile);
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    res.status(500).json({ error: "Failed to fetch profile" });
+  }
+});
+
+spotifyRouter.get("/top/artists", async (req: UserInfo, res) => {
+  const id = req.user.spotifyId;
+  const user = await prisma.user.findFirst({
+    where: {
+      spotifyId: id,
+    },
+  });
+  const accessToken = user.accessToken;
+  const url = "https://api.spotify.com/v1/me/top/artists";
+  const options = {
+    method: "GET",
+    url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      time_range: "medium_term",
+      limit: 10,
+    },
+  };
+
+  try {
+    const response = await axios(options);
+    const topArtists = response.data.items.map((data) => {
+      return data.name;
+    });
+    res.json(topArtists);
+  } catch (error) {
+    console.error("Error fetching top artists:", error);
+    res.status(500).json({ error: "Failed to fetch top artists" });
   }
 });
