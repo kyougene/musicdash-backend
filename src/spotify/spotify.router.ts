22b71309a2369f1a621888a7e8704cd022382da9
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 11:43:44 +0900

    top artist endpoint

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -1,7 +1,30 @@
 import express from "express";
-import { isAuthenticated } from "../authentication.js";
+import axios from "axios";
 export const spotifyRouter = express.Router();
 
-spotifyRouter.get("/recents", isAuthenticated, (req, res) => {
-  res.send(req.user);
+spotifyRouter.get("/top", async (req, res) => {
+  const accessToken =
+    " BQA0G2w_c8j1AJjJ6fLtdE_LjEFf1_UBk1KdiJUT0FmhsRWLQjqI9aJZAQSu_JmYECwCJYijOCTTCS12qLknA3Rr8pxwRFycjrVTaAq14FaqqCj9zUxAwCj5uEHUSmBb0Bs6qlbdJUP8-Rs3pZUFqlyPHpH-CygA58WMqHaYFWh2bofBBahtmbzafdUdczEoayWEhqM";
+
+  const url = "https://api.spotify.com/v1/me/top/artists";
+
+  const headers = {
+    Authorization: `Bearer ${accessToken}`, // Include the user's access token in the request headers
+  };
+
+  const payload = {
+    method: "get",
+    url,
+    headers: headers,
+    params: {
+      time_range: "short_term",
+      limit: 10,
+    },
+  };
+  try {
+    const result = await axios(payload);
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  }
 });
