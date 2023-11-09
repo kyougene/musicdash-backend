45c74caf3cc3fc8628a5c3f92d62448efd477182
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 13:55:38 +0900

    fix

--- src/oauth/auth.router.ts
+++ src/oauth/auth.router.ts
@@ -14,7 +14,6 @@ authRouter.get(
   "/spotify/redirect",
   passport.authenticate("spotify"),
   (req, res) => {
-    console.log(req.user);
     res.status(200).redirect(`${process.env.URL}dashboard`);
   }
 );
