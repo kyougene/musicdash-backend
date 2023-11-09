e5904fe177c475da5479aca7b725dbaac71d0868
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 17:15:32 +0900

    feat: implement top tracks end point

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -1,9 +1,36 @@
 import express from "express";
 import { UserAuthInfoRequest } from "../custom.js";
-
+import prisma from "../db.js";
+import axios from "axios";
 export const spotifyRouter = express.Router();
 
-spotifyRouter.get("/top", (req: UserAuthInfoRequest, res) => {
-  console.log(req.user.spotifyId);
-  res.send(req.user);
+spotifyRouter.get("/top", async (req: UserAuthInfoRequest, res) => {
+  const id = req.user.spotifyId;
+  const user = await prisma.user.findFirst({
+    where: {
+      spotifyId: id,
+    },
+  });
+  const accessToken = user.accessToken;
+  const url = "https://api.spotify.com/v1/me/top/tracks";
+  const options = {
+    method: "get",
+    url,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      time_range: "short_term",
+      limit: 10,
+    },
+  };
+
+  try {
+    const response = await axios(options);
+    const topTracks = response.data.items;
+    res.json(topTracks);
+  } catch (error) {
+    console.error("Error fetching top tracks:", error);
+    res.status(500).json({ error: "Failed to fetch top tracks" });
+  }
 });
