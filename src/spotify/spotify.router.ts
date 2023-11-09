c5a69634f7f36db339df13b5aa95f5d715a48e0e
Kai Eugene <kai.yougene@gmail.com>
2023-11-09 18:05:59 +0900

    fix

--- src/spotify/spotify.router.ts
+++ src/spotify/spotify.router.ts
@@ -5,33 +5,33 @@ import axios from "axios";
 export const spotifyRouter = express.Router();
 
 spotifyRouter.get("/top", async (req: UserInfo, res) => {
-  console.log(req.user.accessToken);
-  const id = req.user.spotifyId;
-  const user = await prisma.user.findFirst({
-    where: {
-      spotifyId: id,
-    },
-  });
-  const accessToken = user.accessToken;
-  const url = "https://api.spotify.com/v1/me/top/tracks";
-  const options = {
-    method: "get",
-    url,
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-    params: {
-      time_range: "short_term",
-      limit: 10,
-    },
-  };
+  console.log(req.user);
+  //   const id = req.user.spotifyId;
+  //   const user = await prisma.user.findFirst({
+  //     where: {
+  //       spotifyId: id,
+  //     },
+  //   });
+  //   const accessToken = user.accessToken;
+  //   const url = "https://api.spotify.com/v1/me/top/tracks";
+  //   const options = {
+  //     method: "get",
+  //     url,
+  //     headers: {
+  //       Authorization: `Bearer ${accessToken}`,
+  //     },
+  //     params: {
+  //       time_range: "short_term",
+  //       limit: 10,
+  //     },
+  //   };
 
-  try {
-    const response = await axios(options);
-    const topTracks = response.data.items;
-    res.json(topTracks);
-  } catch (error) {
-    console.error("Error fetching top tracks:", error);
-    res.status(500).json({ error: "Failed to fetch top tracks" });
-  }
+  //   try {
+  //     const response = await axios(options);
+  //     const topTracks = response.data.items;
+  //     res.json(topTracks);
+  //   } catch (error) {
+  //     console.error("Error fetching top tracks:", error);
+  //     res.status(500).json({ error: "Failed to fetch top tracks" });
+  //   }
 });
